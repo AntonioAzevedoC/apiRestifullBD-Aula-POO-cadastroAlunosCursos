70ff49256024463240bffdcf700096a03403d11c
AntonioAzevedoC <antonioazevedocabreira@gmail.com>
2025-06-03 21:07:48 -0300

    Initial commit

---
+++ src/main/java/com/example/apirestfullbdcadastroalunoscurso/ApiRestfullBdCadastroAlunosCursoApplication.java
@@ -0,0 +1,13 @@
+package com.example.apirestfullbdcadastroalunoscurso;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiRestfullBdCadastroAlunosCursoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiRestfullBdCadastroAlunosCursoApplication.class, args);
+    }
+
+}
