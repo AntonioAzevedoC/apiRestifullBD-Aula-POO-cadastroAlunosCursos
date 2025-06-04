67eba715d949d806b1e0dcc2e12cf25ea119a32e
AntonioAzevedoC <antonioazevedocabreira@gmail.com>
2025-06-04 12:25:14 -0300

    Adicionando controllers

--- src/main/java/com/example/apirestfullbdcadastroalunoscurso/controller/AlunoController.java
+++ src/main/java/com/example/apirestfullbdcadastroalunoscurso/controller/AlunoController.java
@@ -1,4 +1,48 @@
 package com.example.apirestfullbdcadastroalunoscurso.controller;
 
+import com.example.apirestfullbdcadastroalunoscurso.model.Aluno;
+import com.example.apirestfullbdcadastroalunoscurso.service.AlunoService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/alunos")
 public class AlunoController {
+    AlunoService alunoService;
+
+    // Constructor
+    public AlunoController(AlunoService alunoService) {
+        this.alunoService = alunoService;
+    }
+
+    // Get mapping, método para listar todos os alunos
+    @GetMapping
+    public List<Aluno> listarAlunos() {
+        return alunoService.getAlunos();
+    }
+
+    // Get mapping, método para mostrar dados de um aluno
+    @GetMapping("/{id}")
+    public Aluno mostrarAluno(@PathVariable Long id) {
+        return alunoService.getAluno(id);
+    }
+
+    // Post mapping, método para adicionar um aluno
+    @PostMapping
+    public Aluno cadastrarAluno(@RequestBody Aluno aluno) {
+        return alunoService.saveAluno(aluno);
+    }
+
+    // Delete mapping, método para remover um aluno
+    @DeleteMapping("/{id}")
+    public void deletarAluno(@PathVariable Long id) {
+        alunoService.deleteAluno(id);
+    }
+
+    // Put mapping, método para atualizar dados de aluno
+    @PutMapping("/{id}")
+    public Aluno atualizarAluno(@PathVariable Long id, @RequestBody Aluno aluno) {
+        return alunoService.updateAluno(id, aluno);
+    }
 }
--- src/main/java/com/example/apirestfullbdcadastroalunoscurso/controller/CursoController.java
+++ src/main/java/com/example/apirestfullbdcadastroalunoscurso/controller/CursoController.java
@@ -1,4 +1,42 @@
 package com.example.apirestfullbdcadastroalunoscurso.controller;
 
+import com.example.apirestfullbdcadastroalunoscurso.model.Curso;
+import com.example.apirestfullbdcadastroalunoscurso.service.CursoService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/curso")
 public class CursoController {
+    CursoService cursoService;
+
+    // Constructor
+    public CursoController(CursoService cursoService) {
+        this.cursoService = cursoService;
+    }
+
+    // Get mapping, método para listar todos os cursos
+    @GetMapping
+    public List<Curso> listarCurso() {
+        return cursoService.getCursos();
+    }
+
+    // Get mapping, método para mostrar dados de um curso
+    @GetMapping("/{id}")
+    public Curso mostrarCurso(@PathVariable Long id) {
+        return cursoService.getCurso(id);
+    }
+
+    // Post mapping, método para adicionar um curso
+    @PostMapping
+    public Curso cadastrarCurso(@RequestBody Curso curso) {
+        return cursoService.saveCurso(curso);
+    }
+
+    // Put mapping, método para atualizar dados de curso
+    @PutMapping("/{id}")
+    public Curso atualizarCurso(@PathVariable Long id, @RequestBody Curso curso) {
+        return cursoService.updateCurso(id, curso);
+    }
 }
